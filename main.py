5ddc0a2e7118020780ec6454571fd84d14fb623a
Uconcept Designs <85025959+UconceptDesigns@users.noreply.github.com>
2021-08-15 12:22:14 -0400

    Added another function

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ width = 10
 height = 12
 
 print(area_calc(width, height))
+
+
+# this version is better because it will accept
+# both lists and dictionaries.
+
+def mean(value):
+    if type(value) == dict:  # comparing datatype to dic (dictionary datatype)
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+    return the_mean
+
+
+student_grades = {"Marry": 9.1, "Jose": 38, "John": 7.5}
+student_scores = [9.1, 38, 8.8, 7.5]
+
+print(mean(student_grades))  # average from dictionary
+
+print(mean(student_scores))  # average from list
